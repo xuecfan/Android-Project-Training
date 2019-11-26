c1fc5d1f048ecfc897fdb95c09db2a90380230e8
Your Nameyangxitao2017 <707840960@qq.com>
2019-11-26 16:54:20 +0800

    .

--- Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/fragment/My.java
+++ Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/fragment/My.java
@@ -8,15 +8,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.example.chaofanteaching.R;
 import com.example.chaofanteaching.myself.MyData;
+import com.example.chaofanteaching.sign.LoginActivity;
 
 public class My extends Fragment {
     private LinearLayout myself;
     private LinearLayout student;
     private LinearLayout about;
+    private Button btn;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -24,11 +27,36 @@ public class My extends Fragment {
         myself=view.findViewById(R.id.myself);
         student=view.findViewById(R.id.student);
         about=view.findViewById(R.id.about);
+        btn=view.findViewById(R.id.btn);
         myself.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent();
                 i.setClass(getContext(), MyData.class);
+                startActivity(i);
+            }
+        });
+        student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent();
+               // i.setClass(getContext(), .class);
+                startActivity(i);
+            }
+        });
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent();
+              //  i.setClass(getContext(), .class);
+                startActivity(i);
+            }
+        });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent();
+                i.setClass(getContext(), LoginActivity.class);
                 startActivity(i);
             }
         });
--- Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/myself/MyData.java
+++ Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/myself/MyData.java
@@ -37,10 +37,8 @@ public class MyData extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_my_data);
-
-
+        
         photo= findViewById(R.id.image);
         photo2=  findViewById(R.id.img2);
         bt = findViewById(R.id.aaa);
@@ -84,7 +82,6 @@ public class MyData extends AppCompatActivity {
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 // 激活系统图库，选择一张图片
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
@@ -98,16 +95,6 @@ public class MyData extends AppCompatActivity {
      * 剪切图片
      */
     private void crop(Uri uri) {
-//看到有人问裁剪的路径
-// 获取系统时间 然后将裁剪后的图片保存至指定的文件夹
-
-//SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyyMMddhhmmss");
-//String address = sDateFormat.format(new java.util.Date());
-//imagePath = address + ".JPEG";
-//Uri imageUri = Uri.parse("保存的文件夹的名称/" + address
-//         + ".JPEG");
-
-
         // 裁剪图片意图
         Intent intent = new Intent("com.android.camera.action.CROP");
         intent.setDataAndType(uri, "image/*");
@@ -122,14 +109,6 @@ public class MyData extends AppCompatActivity {
         intent.putExtra("outputFormat", "JPEG");// 图片格式
         intent.putExtra("noFaceDetection", true);// 取消人脸识别
         intent.putExtra("return-data", true);
-
-
-//    Cursor cursor = LocationApplication.getContext().getContentResolver().query(uri, null, null, null, null);
-//    if (cursor != null && cursor.moveToFirst()) {
-//        photopath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
-//        Log.e("photopath", "photopath:------------" + photopath); //}
-//filePath=photopath;
-//定义一个全局变量，接受photopath
 
         // 开启一个带有返回值的Activity，请求码为PHOTO_REQUEST_CUT
         startActivityForResult(intent, PHOTO_REQUEST_CUT);
