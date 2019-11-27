1fd50787173f31e43749c7118c43b110614cb451
xuecfan <1339901870@qq.com>
2019-11-27 14:26:34 +0800

    新建验证验证码页面及java文件

---
+++ Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/sign/TestCodeActivity.java
@@ -0,0 +1,15 @@
+package com.example.chaofanteaching.sign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.chaofanteaching.R;
+
+public class TestCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_code);
+    }
+}
