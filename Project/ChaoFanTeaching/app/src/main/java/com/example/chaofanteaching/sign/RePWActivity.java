86b5c656cf1d758b3b72cb989a7206d42b706a57
xuecfan <1339901870@qq.com>
2019-11-27 15:26:13 +0800

    提交忘记密码相关页面

--- Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/sign/RePWActivity.java
+++ Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/sign/RePWActivity.java
@@ -1,7 +1,11 @@
 package com.example.chaofanteaching.sign;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.chaofanteaching.R;
 
@@ -10,6 +14,28 @@ public class RePWActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_code);
+        setContentView(R.layout.activity_repw);
+        //获取id
+        Button backToFindMyPW = findViewById(R.id.backToFindMyPW);
+        EditText myNewPW = findViewById(R.id.myNewPW);
+        Button rePW = findViewById(R.id.rePW);
+
+        //返回找回密码页
+        backToFindMyPW.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //修改密码完成，返回登陆页
+        rePW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(RePWActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
