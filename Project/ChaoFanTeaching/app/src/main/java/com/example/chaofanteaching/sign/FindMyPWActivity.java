c2fe5c894521b1372c129fa73732cc4b4283a6f9
xuecfan <1339901870@qq.com>
2019-11-27 11:17:49 +0800

    更新找回密码

--- Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/sign/FindMyPWActivity.java
+++ Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/sign/FindMyPWActivity.java
@@ -2,6 +2,9 @@ package com.example.chaofanteaching.sign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.chaofanteaching.R;
 
@@ -11,5 +14,19 @@ public class FindMyPWActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_my_pw);
+        //获取id
+        Button backToLogin = findViewById(R.id.backToLogin);
+        EditText myId = findViewById(R.id.myId);
+        EditText myCode = findViewById(R.id.myCode);
+        Button sendCode = findViewById(R.id.sendCode);
+        //返回登录页
+        backToLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //
     }
 }
