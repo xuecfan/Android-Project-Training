ae658ddd3133bc671532a00129e2e142fb64c6da
Your Nameyangxitao2017 <707840960@qq.com>
2019-12-02 17:15:30 +0800

    头像

--- Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/fragment/My.java
+++ Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/fragment/My.java
@@ -2,12 +2,17 @@ package com.example.chaofanteaching.fragment;
 
 import android.Manifest;
 
+import android.annotation.SuppressLint;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -20,6 +25,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 
 
+import android.support.v4.content.FileProvider;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -40,17 +46,23 @@ import com.example.chaofanteaching.about.Student_Authentication;
 import com.example.chaofanteaching.myself.MyData;
 import com.example.chaofanteaching.sign.LoginActivity;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 
 public class My extends Fragment {
 
+    private static String path = "/sdcard/myHead/";// sd路径
+    protected static Uri tempUri;
     private BottomPopupOption bottomPopupOption;
     private ImageButton myself;
     private ImageButton student;
     private ImageButton about;
     private Button btn;
     private ImageView image;
+    private Bitmap bitmap;
+    private Bitmap head;
     private static final int PHOTO_REQUEST_GALLERY = 2;// 从相册中选择
     //private static final int PHOTO_REQUEST_CUT = 3;// 结果
     //private File tempFile;
@@ -79,8 +91,10 @@ public class My extends Fragment {
                                 Intent cameraIntent =
                                         new Intent(
                                                 MediaStore.ACTION_IMAGE_CAPTURE);
-//                                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(
-//                                        Environment.getExternalStorageDirectory(), "hand.jpg")));
+//                                tempUri = FileProvider.getUriForFile(getContext(), getContext().getPackageName() + ".provider", new File(Environment
+//                                        .getExternalStorageDirectory(), "image.jpg"));
+                                // 指定照片保存路径（SD卡），image.jpg为一个临时文件，每次拍照后这个图片都会被替换
+                                //cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempUri);
                                 startActivityForResult(cameraIntent, 8888);
 
 
@@ -163,20 +177,47 @@ public class My extends Fragment {
 //        startActivityForResult(intent, PHOTO_REQUEST_CUT);
 //    }
 
+
+
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == 8888 ) {
             //获取系统摄像头拍照的结果
-            Bitmap bitmap = data.getParcelableExtra("data");
+            bitmap = data.getParcelableExtra("data");
             image.setImageBitmap(bitmap);
+            uploadPic(bitmap);
+
         }
         if (requestCode == PHOTO_REQUEST_GALLERY) {
             try {
-                image.setImageBitmap(MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData()));
+                bitmap= MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData());
+                //setPicToView(bitmap);
+                image.setImageBitmap(bitmap);
+                uploadPic(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+//            if (data != null) {
+//
+//                Bundle extras = data.getExtras();
+//                head = extras.getParcelable("data");
+//                if (head != null) {
+//                    /**
+//                     * 上传服务器代码
+//                     */
+//
+//                    // head = toRoundBitmap1(head);//调用圆角处理方法
+//                    setPicToView(head);// 保存在SD卡中
+//                    image.setImageBitmap(head);// 用ImageView显示出来
+//                    if (head != null && head.isRecycled()) {
+//                        head.recycle();
+//                    }
+//
+//                }
+//            }
+
         }
             // 从相册返回的数据
 
@@ -246,5 +287,119 @@ public class My extends Fragment {
 
 
 
+
+    private void setPicToView(Bitmap mBitmap) {
+        String sdStatus = Environment.getExternalStorageState();
+        if (!sdStatus.equals(Environment.MEDIA_MOUNTED)) { // 检测sd是否可用
+            return;
+        }
+        FileOutputStream b = null;
+        File file = new File(path);
+        file.mkdirs();// 创建文件夹
+        String fileName = path + "head.jpg";// 图片名字
+        try {
+            b = new FileOutputStream(fileName);
+            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, b);// 把数据写入文件
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 关闭流
+                b.flush();
+                b.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // 从本地的文件中以保存的图片中 获取图片的方法
+//    private Bitmap getBitmap(String pathString) {
+//        Bitmap bitmap = null;
+//        try {
+//            File file = new File(pathString);
+//            if (file.exists()) {
+//                bitmap = BitmapFactory.decodeFile(pathString);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return bitmap;
+//    }
+//
+//    @SuppressLint("SdCardPath")
+//    @Override
+//    public void onResume() {
+//        // TODO Auto-generated method stub
+//        super.onResume();
+//        String path1 = "/sdcard/myHead/";// sd路径
+//        bitmap = getBitmap(path1 + "head.jpg");
+//        if (bitmap != null) {
+//            @SuppressWarnings("deprecation")
+//            Drawable drawable = new BitmapDrawable(bitmap);
+//            image.setImageDrawable(drawable);
+//        }
+//    }
+
+    private void uploadPic(Bitmap bitmap) {
+        // 上传至服务器
+        // ... 可以在这里把Bitmap转换成file，然后得到file的url，做文件上传操作
+        // 注意这里得到的图片已经是圆形图片了
+        // bitmap是没有做个圆形处理的，但已经被裁剪了
+
+
+        String imagePath = savePhoto(bitmap, Environment
+                .getExternalStorageDirectory().getAbsolutePath(), String
+                .valueOf(System.currentTimeMillis()));
+        Log.e("imagePath", imagePath+"");
+        if(imagePath != null){
+            // 拿着imagePath上传了
+            // ...
+        }
+    }
+
+
+    public static String savePhoto(Bitmap photoBitmap, String path,
+                                   String photoName) {
+        String localPath = null;
+        if (android.os.Environment.getExternalStorageState().equals(
+                android.os.Environment.MEDIA_MOUNTED)) {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            File photoFile = new File(path, photoName + ".png");
+            FileOutputStream fileOutputStream = null;
+            try {
+                fileOutputStream = new FileOutputStream(photoFile);
+                if (photoBitmap != null) {
+                    if (photoBitmap.compress(Bitmap.CompressFormat.PNG, 100,
+                            fileOutputStream)) { // 转换完成
+                        localPath = photoFile.getPath();
+                        fileOutputStream.flush();
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                photoFile.delete();
+                localPath = null;
+                e.printStackTrace();
+            } catch (IOException e) {
+                photoFile.delete();
+                localPath = null;
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (fileOutputStream != null) {
+                        fileOutputStream.close();
+                        fileOutputStream = null;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return localPath;
+    }
 
 }
