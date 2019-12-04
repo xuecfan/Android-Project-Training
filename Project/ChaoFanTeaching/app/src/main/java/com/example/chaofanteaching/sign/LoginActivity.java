496a9a337d432a8863140eb73d9f54fe5e9fb52d
xuecfan <1339901870@qq.com>
2019-12-04 21:51:54 +0800

    提交登陆注册与数据库连接功能

--- Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/sign/LoginActivity.java
+++ Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/sign/LoginActivity.java
@@ -3,7 +3,10 @@ package com.example.chaofanteaching.sign;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,11 +14,17 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.chaofanteaching.All;
+import com.example.chaofanteaching.HttpConnectionUtils;
 import com.example.chaofanteaching.R;
+import com.example.chaofanteaching.StreamChangeStrUtils;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText myId;
     private EditText myPW;
+    private Handler handler;
 
 
     @Override
@@ -38,13 +47,15 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if (myId.getText().toString().isEmpty() || myPW.getText().toString().isEmpty()){
+                String myid = myId.getText().toString();
+                String mypw = myPW.getText().toString();
+                String IdAndPW = myid +","+ mypw;
+                System.out.println(myid+"   "+mypw+"   "+IdAndPW);
+                if ((myid.isEmpty()) || (mypw.isEmpty())){
                     Toast.makeText(getApplication(),"邮箱和密码不能为空",Toast.LENGTH_LONG).show();
                 } else{
-                    Intent intent = new Intent();
-                    intent.setClass(LoginActivity.this, All.class);
-                    startActivity(intent);
-                    finish();
+                    testUser(myid,mypw);
+
                 }
 
             }
@@ -77,6 +88,52 @@ public class LoginActivity extends AppCompatActivity {
             getWindow().setStatusBarColor(getResources().getColor(R.color.white));//设置状态栏颜色
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//实现状态栏图标和文字颜色为暗色
         }
+    }
+
+    private void testUser(final String a, final String b){
+        handler = new Handler(){
+            public void handleMessage(android.os.Message message){
+                switch (message.what){
+                    case 1:
+                        String string = message.obj.toString();
+                        String para = String.valueOf(1);
+                        System.out.println("从服务器传来的servlet页面数字："+string);
+                        if (string.equals(para)){
+                            Toast.makeText(getApplication(),"登陆成功",Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent();
+                            intent.setClass(LoginActivity.this, All.class);
+                            startActivity(intent);
+                            finish();
+                        }else {
+                            Toast.makeText(getApplication(),"用户名或密码错误",Toast.LENGTH_LONG).show();
+                            myPW.setText("");
+                        }
+                        break;
+                }
+            }
+        };
+        new Thread(){
+            HttpURLConnection connection = null;
+            @Override
+            public void run() {
+                try {
+                    connection = HttpConnectionUtils.getConnection("LoginServlet?myid="+a+"&mypw="+b);
+                    int code = connection.getResponseCode();
+                    if(code!=200){
+                        Log.e("error","网络连接失败");
+                    }else{
+                        InputStream inputStream = connection.getInputStream();
+                        String str = StreamChangeStrUtils.toChange(inputStream);
+                        android.os.Message message = Message.obtain();
+                        message.obj = str;
+                        message.what = 1;
+                        handler.sendMessage(message);
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 
 }
--- Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/sign/LogonActivity.java
+++ Project/ChaoFanTeaching/app/src/main/java/com/example/chaofanteaching/sign/LogonActivity.java
@@ -3,6 +3,8 @@ package com.example.chaofanteaching.sign;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,16 +12,21 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.chaofanteaching.HttpConnectionUtils;
 import com.example.chaofanteaching.R;
+import com.example.chaofanteaching.StreamChangeStrUtils;
 
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 
 public class LogonActivity extends AppCompatActivity {
     private int status;//表示身份是家长还是老师，0为家长，1为老师
     private EditText myId;
     private EditText myPW;
+    private EditText myPWAgain;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +41,7 @@ public class LogonActivity extends AppCompatActivity {
         final TextView imTeacher = findViewById(R.id.imTeacher);
         myId = findViewById(R.id.myId);
         myPW = findViewById(R.id.myPW);
-        EditText myPWAgain = findViewById(R.id.myPWAgain);
+        myPWAgain = findViewById(R.id.myPWAgain);
         ImageView logonBtn = findViewById(R.id.logonBtn);
 
         //返回登陆页面
@@ -84,14 +91,18 @@ public class LogonActivity extends AppCompatActivity {
         logonBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
                 String user=myId.getText().toString();
                 String pasd=myPW.getText().toString();
-                addUser(user,pasd,status);
-                finish();
-//                Intent intent = new Intent();
-//                intent.setClass(LogonActivity.this,LoginActivity.class);
-//                startActivity(intent);
+                String pasdAgain = myPWAgain.getText().toString();
+                if ((user.isEmpty()) || (pasd.isEmpty()) || (pasdAgain.isEmpty())){
+                    Toast.makeText(getApplication(),"用户名和密码不能为空",Toast.LENGTH_LONG).show();
+                }else if (!pasd.equals(pasdAgain)){
+                    Toast.makeText(getApplication(),"两次密码不一致",Toast.LENGTH_LONG).show();
+                }else{
+                    addUser(user,pasd,status);
+
+                }
+
             }
         });
     }
@@ -104,8 +115,28 @@ public class LogonActivity extends AppCompatActivity {
         }
     }
 
-    //在数据库里添加用户
+    //在数据库里添加用户，判断是否注册成功
     private void addUser(final String a, final String b, final int c){
+        handler = new Handler(){
+            public void handleMessage(android.os.Message message){
+                switch (message.what){
+                    case 1:
+                        String string = message.obj.toString();
+                        String para = String.valueOf(1);
+                        if (string.equals(para)){
+                            Toast.makeText(getApplication(),"注册成功，请登录",Toast.LENGTH_LONG).show();
+                            finish();
+
+//                            Intent intent = new Intent();
+//                            intent.setClass(LogonActivity.this,LoginActivity.class);
+//                            startActivity(intent);
+                        }else{
+                            Toast.makeText(getApplication(),"注册失败",Toast.LENGTH_LONG).show();
+
+                        }
+                }
+            }
+        };
         new Thread(){
             HttpURLConnection connection = null;
             @Override
@@ -115,6 +146,13 @@ public class LogonActivity extends AppCompatActivity {
                     int code = connection.getResponseCode();
                     if(code!=200){
                         Log.e("error","网络连接失败");
+                    }else {
+                        InputStream inputStream = connection.getInputStream();
+                        String string = StreamChangeStrUtils.toChange(inputStream);
+                        android.os.Message message = Message.obtain();
+                        message.obj = string;
+                        message.what = 1;
+                        handler.sendMessage(message);
                     }
                 }catch (Exception e) {
                     e.printStackTrace();
